515cfca3a5f16f37a67956593326ee6b1c7f4469
hanbin125 <cyhm2545@naver.com>
2025-03-13 10:55:52 +0900

    [Feat] 메인메뉴 기초생성

--- Assets/03.Scripts/2.UI/ChangeScene.cs
+++ Assets/03.Scripts/2.UI/ChangeScene.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void ChangeGameScene()
+    public static void Change(string sceneName)
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(sceneName);
     }
 }
