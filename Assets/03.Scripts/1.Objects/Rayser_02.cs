5a2b78053fd23d7e8c87a6c83778b07c91b39b08
Bosung <gobosung2000@gmail.com>
2025-03-14 11:31:15 +0900

    [Feat] lasercube -작업중_01

--- Assets/03.Scripts/1.Objects/Rayser_02.cs
+++ Assets/03.Scripts/1.Objects/Rayser_02.cs
@@ -20,25 +20,42 @@ public class Rayser_02 : MonoBehaviour
         RaycastHit hit;
 
         //쏘는 위치,방향,결과값,최대인식거리
-        Physics.Raycast(transform.position, transform.forward, out hit, maxDistance);
-        // 시작 지점과 히트 지점 사이의 중간 위치 계산
-        Vector3 middlePosition = transform.position + (hit.point - transform.position) / 2;
-        //레이저를 중간위치에 설정(우리의 sprite는 한쪽방향이 아닌 양쪽으로 증가하기 때문)
-        ScaleDistance.transform.position = middlePosition;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+        {
+            int layserIndex = hit.collider.gameObject.layer;
+            string layerName = LayerMask.LayerToName(layserIndex);
+            Debug.Log("Hit object layer: " + layerName + " (index: " + layserIndex + ")");
 
-        Debug.DrawRay(transform.position,transform.forward* maxDistance, Color.yellow);
+            // 시작 지점과 히트 지점 사이의 중간 위치 계산
+            Vector3 middlePosition = transform.position + (hit.point - transform.position) / 2;
+            //레이저를 중간위치에 설정(우리의 sprite는 한쪽방향이 아닌 양쪽으로 증가하기 때문)
+            ScaleDistance.transform.position = middlePosition;
 
-        // 레이저가 히트 지점을 향하도록 회전
-        //ScaleDistance.transform.LookAt(hit.point);
+            Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.yellow);
 
-        //거리에 따른 레이저 스케일변화 
-        ScaleDistance.transform.localScale = new Vector3(0.1f, hit.distance, 0.1f);
+            // 레이저가 히트 지점을 향하도록 회전
+            //ScaleDistance.transform.LookAt(hit.point);
 
-        //레이캐스트가 땋는곳에 오브젝트를 옮긴다.
-        //RayResult.transform.position = hit.point;
+            //거리에 따른 레이저 스케일변화 
+            ScaleDistance.transform.localScale = new Vector3(0.1f, hit.distance, 0.1f);
 
-        //해당하는 오브젝트의 회전값을 닿은 면적의 노멀방향와 일치시킨다.
-        //RayResult.transform.rotation = Quaternion.LookRotation(hit.normal);
+            //레이캐스트가 땋는곳에 오브젝트를 옮긴다.
+            //RayResult.transform.position = hit.point;
+
+            //해당하는 오브젝트의 회전값을 닿은 면적의 노멀방향와 일치시킨다.
+            //RayResult.transform.rotation = Quaternion.LookRotation(hit.normal);
+
+            //레이저 큐브와 충돌했을때 
+            if (layserIndex ==LayerMask.NameToLayer("LayserCube"))
+            {
+                // hit.point 에서 ray를 다시 쏘기
+                hit.collider.GetComponent<Cube_Rayser>().ChkRayserLayser();
+            }
+            else
+            {
+
+            }
+        }
 
     }
 }
