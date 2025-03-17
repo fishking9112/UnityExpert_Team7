79429dc70bb63173a7c9d480c318ffc07de23efb
Kim199610 <kk2247401@naver.com>
2025-03-17 15:12:57 +0900

    [feat] CrossHair추가

--- Assets/03.Scripts/2.UI/CrossHair.cs
+++ Assets/03.Scripts/2.UI/CrossHair.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,4 +22,5 @@ public class CrossHair : MonoBehaviour
     {
         crossHairBlue.enabled = value;
     }
+    
 }
