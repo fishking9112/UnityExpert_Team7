25cbda7c6a33212a3547db72c9e1d3d8b5e2b408
Kim199610 <kk2247401@naver.com>
2025-03-18 14:42:20 +0900

    [feat]스테이지 끝 구조물 만드는중

---
+++ Assets/03.Scripts/1.Objects/Btn/StageEndButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEndButton : Button
+{
+    // Start is called before the first frame update
+    public override void ChkedPress()
+    {
+        base.ChkedPress();
+
+    }
+}
