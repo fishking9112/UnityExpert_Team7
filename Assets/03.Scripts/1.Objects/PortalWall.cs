48a11d61f77a6eaad66bf41b1e28ef4cda687709
Kim199610 <kk2247401@naver.com>
2025-03-19 09:48:00 +0900

    [fix] BasePortalAble, PortalWall 스크립트 주석추가

--- Assets/03.Scripts/1.Objects/PortalWall.cs
+++ Assets/03.Scripts/1.Objects/PortalWall.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PortalWall : BasePortalAble
 {
-    public override Vector3 SummonPortal(Vector3 hitPosition)
+    public override Vector3 SummonPortal(Vector3 hitPosition)           //포탈이 소환될 위치에 맞춰 콜라이더 크기 조정(포탈 크기 구멍 만듦)
     {
         Vector3 localHitPosition = transform.InverseTransformPoint(hitPosition);
-        Vector3 summonPosition = new Vector3(localHitPosition.x, Mathf.Clamp(localHitPosition.y, 1.5f, 7.5f), Mathf.Clamp(localHitPosition.z, 0.8f, 5.2f));
+        Vector3 summonPosition = new Vector3(localHitPosition.x, Mathf.Clamp(localHitPosition.y, 1.5f, 7.5f), Mathf.Clamp(localHitPosition.z, 0.8f, 5.2f)); //포탈 생성 가능 좌표를 보정해서 반환(모서리같은데)
 
-        colliders[0].size = new Vector3(0.3f, 9f - (summonPosition.y + 1.5f), 1.6f);
+        colliders[0].size = new Vector3(0.3f, 9f - (summonPosition.y + 1.5f), 1.6f);                        //생성 위치에 맞춰 콜라이더들 크기조정
         colliders[0].center = new Vector3(0,(9f + summonPosition.y + 1.5f) / 2f, summonPosition.z);
         colliders[1].size = new Vector3(0.6f, summonPosition.y - 1.5f, 1.6f);
         colliders[1].center = new Vector3(0, (summonPosition.y - 1.5f) / 2f, summonPosition.z);
@@ -18,7 +18,7 @@ public class PortalWall : BasePortalAble
         colliders[3].size = new Vector3(0.3f, 9f, 6f - (summonPosition.z + 0.8f));
         colliders[3].center = new Vector3(0, 4.5f, (6f + summonPosition.z + 0.8f) / 2f);
 
-        return transform.TransformPoint(summonPosition);
+        return transform.TransformPoint(summonPosition);            //포탈생성위치(보정됨) 반환
     }
 
 }
