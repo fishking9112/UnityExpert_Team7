7869380bfafea16b263d5e6712fdd223f624e720
Kim199610 <kk2247401@naver.com>
2025-03-19 09:39:30 +0900

    [fix] Portal, PlayerController 스크립트 주석 추가

--- Assets/03.Scripts/0.Player/PortalGun.cs
+++ Assets/03.Scripts/0.Player/PortalGun.cs
@@ -67,7 +67,7 @@ public class PortalGun : MonoBehaviour
         gunScript = gun;
     }
 
-    void CheckPotalAble()
+    void CheckPotalAble()           //포탈이 소환가능한지 여부를 판단  ==>>  조건 : <BasePortalAble>스크립트 소지, 해당 물체(벽)에 이미 열려있는 다른 종류(색) 포탈 없어야함
     {
         ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         if (Physics.Raycast(ray, out hit, 2000f, canShotPotalLayerMask) && hit.transform.TryGetComponent<BasePortalAble>(out wall))
@@ -84,7 +84,7 @@ public class PortalGun : MonoBehaviour
 
     public void OnShotRedPortal(InputAction.CallbackContext context)
     {
-        if (!canShotPortal || isPause)
+        if (!canShotPortal || isPause)                  //포탈건 미소지,일시정지상태 일때 발사금지
             return;
         if(context.performed)
         {
@@ -94,13 +94,13 @@ public class PortalGun : MonoBehaviour
             }
             gunScript.ShootAnimation(true);
 
-            if (canShotRedPortal)
+            if (canShotRedPortal)                       //포탈 소환 가능 조건검사
             {
-                redWall?.SetMainCollider(true);
-                redWall = wall;
-                redWall.SetMainCollider(false);
-                Vector3 summonPos = redWall.SummonPortal(hit.point);
-                redPortal.SummonPortal(summonPos, hit.normal);
+                redWall?.SetMainCollider(true);         //원래 해당포탈(red)이 소환되있던 벽의 콜라이더 정상화
+                redWall = wall;                         //소환될 벽 저장
+                redWall.SetMainCollider(false);         //소환될 벽 콜라이더 변형(포탈만한 콜라이더 구멍생성)
+                Vector3 summonPos = redWall.SummonPortal(hit.point);    //벽에서 보정된 소환가능 위치 반환 (모서리에 쏘면 안쪽으로 자동 보정됨)
+                redPortal.SummonPortal(summonPos, hit.normal);          //포탈소환
             }
             
         }
@@ -123,7 +123,7 @@ public class PortalGun : MonoBehaviour
         //}
 
     }
-    public void OnShotBluePortal(InputAction.CallbackContext context)
+    public void OnShotBluePortal(InputAction.CallbackContext context)   //위(red)와 같음
     {
         if (!canShotPortal || isPause)
             return;
@@ -169,7 +169,7 @@ public class PortalGun : MonoBehaviour
     {
         isPause = value;
     }
-    public void ActivatePortalGun(bool value)
+    public void ActivatePortalGun(bool value)       //포탈건 집어야 포탈 발사가능
     {
          canShotPortal = value;
     }
--- Assets/03.Scripts/1.Objects/Portal.cs
+++ Assets/03.Scripts/1.Objects/Portal.cs
@@ -29,15 +29,15 @@ public class Portal : MonoBehaviour
         SetCameraPositon();
         //CameraUpdate();
     }
-    void SetCameraPositon()
+    void SetCameraPositon()             //반대측(반대색깔) 포탈에 비칠 풍경을 찍을 카메라 위치 세팅
     {
-        Vector3 localPos = otherPotal.transform.InverseTransformPoint(player.cameraContainer.position);
-        localPos.z = -localPos.z;
+        Vector3 localPos = otherPotal.transform.InverseTransformPoint(player.cameraContainer.position);     //반대측 포탈 기준 플레이어의 local좌표 계산
+        localPos.z = -localPos.z;                                                                           //계산된 local좌표를 y축으로 180도 회전
         localPos.x = -localPos.x;
         portalCamera.transform.localPosition = localPos;
-        CameraProjectionUpdate();
+        CameraProjectionUpdate();                                                                           //중요중요중요, 카메라의 "절두체"를 포탈과의 거리,각도에 맞춰 조정
         Vector3 lookPoint = transform.position + (0.01f * transform.forward);
-        portalCamera.transform.localRotation = Quaternion.identity;
+        portalCamera.transform.localRotation = Quaternion.identity;                                         //카메라 회전은 0으로 고정
 
         //portalCamera.transform.localRotation = FindLookRotate(-portalCamera.transform.localPosition,transform.up);
         //portalCamera.transform.LookAt(lookPoint);
@@ -53,7 +53,7 @@ public class Portal : MonoBehaviour
 
 
 
-    void CameraProjectionUpdate()
+    void CameraProjectionUpdate()                                       //카메라 절두체 수동 조정(핵심 이지만 글,말로 설명은 못함) portalCamera.projectionMatrix 를 수동으로 조정
     {
         Vector3 localPos = portalCamera.transform.localPosition;
         float right = -localPos.x + (portalWidth / 2f);
@@ -86,7 +86,7 @@ public class Portal : MonoBehaviour
     }
 
 
-    public void SummonPortal(Vector3 hitPoint, Vector3 hitNormal)
+    public void SummonPortal(Vector3 hitPoint, Vector3 hitNormal)           //포탈 소환 ==>> 포탈의 각도 조정 z축 회전은 0으로 고정
     {
 
 
@@ -95,13 +95,32 @@ public class Portal : MonoBehaviour
         IsConnected();
 
 
-        Vector3 forward = hitNormal;
-        //Vector3 forward = isRedPortal ? hitNormal : -hitNormal;
-        Vector3 right = Vector3.Cross(Vector3.up, forward);
-        Vector3 up = Vector3.Cross(forward, right);
-
+        Vector3 forward = hitNormal;                                        //포탈 forward방향은 소환벽면의 법선과 일치 (수직)
+        
+        Vector3 right = Vector3.Cross(Vector3.up, forward);                 //포탈의 x축(=right)방향을 월드up방향과 포탈의 forward방향과 수직되게 설정
+        Vector3 up = Vector3.Cross(forward, right);                         //포탈의 y축(=up)방향을 계산해둔 포탈 x축(right), z축(up)방향과 수직되게 설정
+                                                                                        // == 포탈의 z회전을 0으로 고정 
         transform.rotation = Quaternion.LookRotation(forward, up);
 
+
+       
+
+        transform.position = hitPoint;                                      //포탈이 생성될 위치
+
+        plane = new Plane(transform.forward, transform.position);           //포탈 통과할때 이동시킬 기준이 될 평면 설정 (해당평면을 지나칠때 이동시킴)
+
+        bool isPortalOpen = otherPotal.gameObject.activeSelf;               //다른 포탈도 활성화 상태일때만 서로 연결된 이미지,기능을 사용가능
+        collider1.isTrigger = isPortalOpen;                                 //플레이어와 오브젝트(큐브 등)을 인식할 콜라이더를 따로 설정(범위문제)
+        collider2.enabled = isPortalOpen;
+        otherPotal.collider1.isTrigger = isPortalOpen;
+        otherPotal.collider2.enabled = isPortalOpen;
+        
+        animator.SetTrigger("PortalOpen");
+
+
+        //===================================================================== 폐기된 코드들,모아둠
+
+        //Vector3 forward = isRedPortal ? hitNormal : -hitNormal;
 
         //Vector3 forward = isRedPortal? hitNormal : -hitNormal;
         //Vector3 right = Vector3.Cross(Vector3.up, forward);
@@ -113,19 +132,9 @@ public class Portal : MonoBehaviour
         //float rotationY = Vector3.SignedAngle(Vector3.forward * (isRedPortal ? 1f: -1f), hitNormal, Vector3.up);
         //float rotationX = 90f - Vector3.Angle(Vector3.up, hitNormal);
 
-        transform.position = hitPoint;
-
-        plane = new Plane(transform.forward, transform.position);
-
-        bool isPortalOpen = otherPotal.gameObject.activeSelf;
-        collider1.isTrigger = isPortalOpen;
-        collider2.enabled = isPortalOpen;
-        otherPotal.collider1.isTrigger = isPortalOpen;
-        otherPotal.collider2.enabled = isPortalOpen;
         //transform.rotation = Quaternion.Euler(isRedPortal? rotationX : -rotationX, rotationY, 0);
-        animator.SetTrigger("PortalOpen");
     }
-    void IsConnected()
+    void IsConnected()                                          //포탈이 둘다 열리지 않을경우 연결되지않음을 나타내는 이미지 출력
     {
         bool connected = otherPotal.gameObject.activeSelf;
         notConnectImg.enabled = !connected;
@@ -135,40 +144,40 @@ public class Portal : MonoBehaviour
     //{
     //    other.transform.SetParent(transform);
     //}
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other)                              //포탈간 이동에 관한 코드 !!!!!
     {
-        if (((1 << other.gameObject.layer) & canTelefortLayerMask) == 0)
+        if (((1 << other.gameObject.layer) & canTelefortLayerMask) == 0)            //이동가능한 물체 레이어 검사
             return;
 
-        if (other.TryGetComponent<Cube>(out Cube cube) && cube.IsPickedUp)
+        if (other.TryGetComponent<Cube>(out Cube cube) && cube.IsPickedUp)          //e로 집은 큐브는 제외
             return;
 
-        if (plane.GetDistanceToPoint(other.transform.position) >= 0)
+        if (plane.GetDistanceToPoint(other.transform.position) >= 0)                //아까 설정한 포탈 평면과 이동될 물체의 좌표의 거리가 0 미만일때 다른포탈로 전송
             return;
 
-        other.transform.SetParent(transform);
-        other.transform.SetParent(otherPotal.transform, false);
+        other.transform.SetParent(transform);                                           //   이동시킬 물체를 이 포탈의 자식으로설정
+        other.transform.SetParent(otherPotal.transform, false);                         // local Position, local rotation을 고정시킨채로 다른포탈의 자식으로 재설정(==다른포탈 장소로 순간이동)
         Vector3 localPos = other.transform.localPosition;
-        other.transform.localPosition = new Vector3(-localPos.x, localPos.y, -localPos.z);
+        other.transform.localPosition = new Vector3(-localPos.x, localPos.y, -localPos.z);  //(포탈에서 나오는 방향으로) y축으로 180도 회전시킨 position로 조정
 
 
-        Vector3 localEuler = other.transform.localRotation.eulerAngles;
+        Vector3 localEuler = other.transform.localRotation.eulerAngles;                     //(포탈에서 나오는 방향으로) y축으로 180도 회전시킨 rotation으로 조정
         localEuler.y = localEuler.y + 180f;
         other.transform.localRotation = Quaternion.Euler(localEuler);
 
 
-        Rigidbody rb = other.GetComponent<Rigidbody>();
+        Rigidbody rb = other.GetComponent<Rigidbody>();                                     //(포탈에서 나오는 방향으로) y축으로 180도 회전시킨 velocity를 가지도록 조정
         Vector3 velocity = rb.velocity;
         velocity = transform.InverseTransformDirection(velocity);
         velocity = new Vector3(-velocity.x, velocity.y, -velocity.z);
         velocity = otherPotal.transform.TransformDirection(velocity);
-        velocity += (otherPotal.transform.forward * aditionalPortalExitSpeed);
+        velocity += (otherPotal.transform.forward * aditionalPortalExitSpeed);              //나올때 사출속도 추가해줌
         rb.velocity = velocity;
 
         other.transform.SetParent(null);
 
         /*
-        //==============================================================
+        //=========================================================================================== 폐기된 코드들
         if (isRedPortal && plane.GetDistanceToPoint(other.transform.position) >= 0)
             return;
         if (!isRedPortal && plane.GetDistanceToPoint(other.transform.position) <= 0)
@@ -246,7 +255,7 @@ public class Portal : MonoBehaviour
     //    other.transform.SetParent(null);
     //}
 
-    public Vector3 LaserPosition(Vector3 hitPoint)
+    public Vector3 LaserPosition(Vector3 hitPoint)                          //들어온 레이저의 좌표(레이저를 맞은 좌표)를 이용해 (다른쪽 포탈에서)나올 레이저 시작 좌표를 반환 
     {
         Vector3 localHitPoint = transform.InverseTransformPoint(hitPoint);
 
@@ -260,11 +269,8 @@ public class Portal : MonoBehaviour
         return newStartPosition;
 
     }
-    public Vector3 LaserNormalPosition(Vector3 hitPoint)
-    {
-        return transform.position + transform.forward * 0.1f;
-    }
-    public Vector3 LaserDirection(Vector3 hitPoint,Vector3 startPosition)
+    
+    public Vector3 LaserDirection(Vector3 hitPoint,Vector3 startPosition)       //들어오 레이저의 방향을 이용해 (다른포탈에서)나올 레이저의 방향을 반환
     {
         Vector3 laserDirection = hitPoint - startPosition;
         laserDirection = transform.InverseTransformDirection(laserDirection);
@@ -274,6 +280,11 @@ public class Portal : MonoBehaviour
 
         return laserDirection;
     }
+    public Vector3 LaserNormalPosition(Vector3 hitPoint)                //===================  만일 들어가는 각도와 위치에 상관없이 포탈 중앙에서 레이저를 나오게하고싶다면 아래 코드
+    {
+        return transform.position + transform.forward * 0.1f;
+    }
+
     public Vector3 LaserNormalDirection(Vector3 hitPoint, Vector3 startPosition)
     {
         return transform.forward;
